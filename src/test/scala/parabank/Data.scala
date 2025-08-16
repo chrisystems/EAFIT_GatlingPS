7c08b45da3c235bc2ff74bd1db04408dbd45bd5e
chrisystems <51771601+chrisystems@users.noreply.github.com>
2025-08-16 09:49:24 -0500

    Update Data.scala

--- src/test/scala/parabank/Data.scala
+++ src/test/scala/parabank/Data.scala
@@ -4,5 +4,9 @@ object Data{
     val url= "https://parabank.parasoft.com/parabank/services/bank"
     val username = "christianeafit2025"
     val password = "demo"
+    val fromAccountId = "16341"
+    val toAccountId = "19005"
+    val amount = "1"
 
 }
+
